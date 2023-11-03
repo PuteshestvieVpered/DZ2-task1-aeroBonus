6faaff139eec1c56005e126bda5311be4e383fdb
Fokina Alisa <ophiopluteus@mail.ru>
2023-11-03 18:41:26 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ruble = 7898;
+        int coefficient = 20;
+        int bonus = ruble / coefficient;
+        System.out.println("Количество бонусов за билет " + bonus);
+
+    }
+}
